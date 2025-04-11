994d56e2ff07ac05a66833721c2feb196106e133
janijeet17 <janijeet38@gmail.com>
2025-04-12 01:00:25 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
 
 
 createRoot(document.getElementById("root")!).render(<App />);
