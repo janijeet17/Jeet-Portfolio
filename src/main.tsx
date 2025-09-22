86388ad6935966386ed7741f460c464b248e7dd5
janijeet17 <janijeet38@gmail.com>
2025-09-22 18:52:51 +0530

    Refactor main.tsx to remove React.StrictMode wrapper for cleaner rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    {/* 👇 basename must match your repo name */}
-    <BrowserRouter basename="/Jeet-Portfolio">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename="/Jeet-Portfolio">
+    <App />
+  </BrowserRouter>
 );
